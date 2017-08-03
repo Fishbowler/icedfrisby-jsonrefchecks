792880e247dc0764dd8181e562066be45f4adbf5
Dan Caseley <dan@caseley.me.uk>
2017-08-03 16:52:18 +0100

    Add configurable behaviour for zero candidates

--- expectJSONRefDataExists.js
+++ expectJSONRefDataExists.js
@@ -1,18 +1,32 @@
 const jsonpath = require('jsonpath');
 const chai = require('chai');
 
+const defaultConfig = {
+  "debug": false,                 //Would print number of candidates and references
+  "failOnZeroCandidates": false   //Would fail when candidates.length == 0
+}
+
 const factory = superclass => class ExpectJSONRefDataExists extends superclass {
-  expectJSONRefDataExists(candidatePattern, referencePattern) {
+  expectJSONRefDataExists(candidatePattern, referencePattern, config) {
     const self = this;
+    let thisConfig = config ? Object.assign({}, defaultConfig, config) : defaultConfig;
     this.current.expects.push(function() {
       const body = self.current.response.body;
       var bodyJSON = (typeof body === "object") ? body : JSON.parse(body);
       var candidates = jsonpath.query(bodyJSON, candidatePattern);
       var references = jsonpath.query(bodyJSON, referencePattern);
-      var matches = candidates.filter((candidate) => {
-        return (references.includes(candidate));
-      });
-      chai.expect(matches.length).to.equal(candidates.length);
+      if(thisConfig.debug){
+        console.log('Candidates: ' + candidates.length);
+        console.log('References: ' + references.length);
+      }
+      if(thisConfig.failOnZeroCandidates && candidates.length == 0){
+        chai.expect.fail("0 candidates","Some candidates","Query returned zero candidates")
+      } else {
+        var matches = candidates.filter((candidate) => {
+          return (references.includes(candidate));
+        });
+        chai.expect(matches.length).to.equal(candidates.length);
+      }
     });
     return this;
   }
